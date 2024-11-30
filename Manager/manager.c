00a24292d2e703b85c6d0bb81581e6478e3aa0d3
OutOfHandles <marcoandreportela2005@gmail.com>
2024-11-30 19:36:41 +0000

    removed all the commented functions

--- Manager/manager.c
+++ Manager/manager.c
@@ -2,115 +2,12 @@
 
 int fd_manager_fifo;
 
-/*int openUserFifo(pid_t pid){
-    char fifoName[25];
-    sprintf(fifoName, FEED_FIFO, pid);
-    int fifo = open(fifoName, O_WRONLY);
-
-    return fifo;
-}*/
-/*
-void removeUser(Users users[], pid_t pid){
-    int newSize = 0;
-    pthread_mutex_lock(users->mutex);
-    for(int i = 0; i < users->size; i++){
-        if(users->userList[i].pid != pid)
-            users->userList[newSize++] = users->userList[i];
-    }
-    users->size = newSize;
-    pthread_mutex_unlock(users->mutex);
-}
-*/
-/*ManagerStatus addUser(Users *users, char *name, User *newUser){
-    if(users->size >= MAX_USERS){
-        return MANAGER_FULL;
-    }
-    for(int i = 0; i < users->size; i++){
-        if(strcmp(name, users->userList[i].name) == 0) return USERNAME_IN_USE;
-    }
-    ManagerStatus ret = SUCCESS;
-
-    snprintf(newUser->name, MAX_NAME_LEN, "%s", name); //acts like strcpy_s
-
-    if(strlen(name) >= MAX_NAME_LEN)
-        ret = TRUNCATED;
-
-    users->userList[users->size++] = *newUser;
-    return ret;
-}*/
-
-/*void handleLogin(Users *users, char *name, pid_t pid){
-    int fifo = openUserFifo(pid);
-    
-    if(fifo == -1){
-        printf("Failed to open feed fifo\n");
-        kill(pid, SIGUSR1); //USR1 -> internal server error
-        return;
-    }
-
-    User newUser = {.pid = pid, .nTopics = 0};
-
-    pthread_mutex_lock(users->mutex);
-    ManagerStatus status = addUser(users , name, &newUser);
-    pthread_mutex_unlock(users->mutex);
-
-    int written = write(fifo, &status, sizeof(ManagerStatus));
-    
-    if(written <= 0){
-        kill(pid, SIGUSR1);
-        printf("Failed to send response to user\n");
-    }
-
-    close(fifo);
-}*/
-
 void Abort(int code){
     close(fd_manager_fifo);
     unlink(MANAGER_FIFO);
 
     exit(code);
 }
-/*
-bool alreadyExists(Topics *topics, char *topic){
-    for(int i = 0; i < topics->size; i++){
-        if(strcmp(topics->topicsList[i].name, topic) == 0) return true;
-    }
-
-    return false;
-}*/
-/*
-int loadFile(PersistentMessage pm[], int *size, Topics *topics){
-    FILE *f = fopen("messages.txt", "r"); //change to read from env
-
-    if(!f) return -1;
-    int i = 0;
-    while(fscanf(f, "%s %s %d %[^\n]s", pm[i].topic, pm[i].name, &pm[i].lifetime, pm[i].content) != EOF){
-        if(!alreadyExists(topics, pm[i].topic)){
-            strcpy(topics->topicsList[topics->size].name, pm[i].topic);
-            topics->topicsList[topics->size++].locked = false;
-        }
-        i++;
-    }
-    *size = i;
-
-    fclose(f);
-    return 0;
-}*/
-/*
-void saveToFile(PersistentMessage pm[], int size){
-    FILE *f = fopen("messages.txt", "w"); //change to read from env
-
-    if(!f){
-        printf("Failed to open file\n");
-        return;
-    }
-    
-    for(int i = 0; i < size; i++){
-        fprintf(f, "%s %s %d %s\n", pm[i].topic, pm[i].name, pm[i].lifetime, pm[i].content);
-    }
-
-    fclose(f);
-}*/
 
 void handleSignal(int sig){}
 
@@ -129,11 +26,6 @@ void *persistentThread(void *data){
         }
         bool flag = false;
         for(int i = 0; i < topics->nTopics; i++){
-            // pList->messageList[i].lifetime--;
-
-            // if(pList->messageList[i].lifetime <= 0){
-            //     flag = 1;
-            // }
             flag = false;
             for(int j = 0; j < topics->topicsList[i].nPersistent; j++){
                 if(--topics->topicsList[i].persistentList[j].lifetime <= 0){
@@ -143,10 +35,6 @@ void *persistentThread(void *data){
 
             if(flag){
                 int newSize = 0; 
-                // for(int i = 0; i < pList->size; i++){
-                //     if(pList->messageList[i].lifetime > 0)
-                //         pList->messageList[newSize++] = pList->messageList[i]; 
-                // }
                 for(int j = 0; j < topics->topicsList[i].nPersistent; j++){
                     if(topics->topicsList[i].persistentList[j].lifetime > 0)
                         topics->topicsList[i].persistentList[newSize++] = topics->topicsList[i].persistentList[j];
@@ -184,7 +72,7 @@ void *pipeThread(void *data){
 
         if(size == -1) break; //handle == 0???
 
-        /*switch(req.type){
+        switch(req.type){
             case LOGIN: {
                 char buffer[req.size];
                 size = read(fd_manager_fifo, buffer, req.size);
@@ -210,43 +98,13 @@ void *pipeThread(void *data){
             default: {
                 break;
             }
-        }*/
+        }
     }
 
     printf("<MANAGER>: Closing pipe thread\n");
 
     pthread_exit(NULL); 
 }
-
-/*void showUsers(User users[], int size){
-    if(size == 0){
-        printf("<MANAGER>: No users at the moment\n");
-    }
-    for(int i = 0; i < size; i++){
-        printf("<MANAGER>: [%d] -> %s\n", users[i].pid, users[i].name);
-    }
-}*/
-/*void showPersistent(PersistentMessage messages[], int size, char *topic){
-    bool shown = false;
-    for(int i = 0; i < size; i++){
-        if(strcmp(messages[i].topic, topic) == 0 || strcmp(topic, "*") == 0){ //|| strcmp(topic, "*") == 0
-            printf("[%s] : <%s> : %d : %s\n", messages[i].name, messages[i].topic, messages[i].lifetime, messages[i].content);
-            shown = true;
-        }
-    }
-
-    if(!shown) printf("<MANAGER>: No messsages available at the moment\n");
-}*/
-
-/*
-void showTopics(Topic topics[], int size){
-    if(size == 0){
-        printf("<MANAGER>: No topics at the moment\n");
-    }
-    for(int i = 0; i < size; i++){
-        printf("<MANAGER>: %s [%s]\n", topics[i].name, topics[i].locked ? "LOCKED" : "UNLOCKED");
-    }
-}*/
 
 void closeManager(Users *users){
     for(int i = 0; i < users->size; i++){
@@ -256,54 +114,6 @@ void closeManager(Users *users){
     close(fd_manager_fifo);
     unlink(MANAGER_FIFO);
 }
-/*
-void changeTopicState(Topic topics[], int size, char *name, bool state){
-    bool changed = false;
-    for(int i = 0; i < size; i++){
-        if(strcmp(topics[i].name, name) == 0){
-            if(topics[i].locked == state){ 
-                printf("<MANAGER>: Topic already %s\n", state ? "locked" : "unlocked");
-                return;
-            }
-            topics[i].locked = state;
-            changed = true;
-        }
-    }
-    if(!changed) 
-        printf("<MANAGER>: Invalid topic name\n");
-    else
-        printf("<MANAGER>: Topic successfully %s\n", state ? "locked" : "unlocked");
-}
-*/
-
-/*void kickUser(Users *users, int size, char *name){
-    pid_t pid = -1;
-    for(int i = 0; i < size; i++){
-        if(strcmp(name, users->userList[i].name) == 0){
-            pid = users->userList[i].pid;
-            break;
-        }
-    }
-    if(pid == -1){
-        printf("<MANAGER>: Unable to find target user\n");
-        return;
-    }
-
-    Headers h = {.pid = pid, .size = 0, .type = KICK};
-    for(int i = 0; i < size; i++){
-        int fifo = openUserFifo(users->userList[i].pid);
-        if(fifo == -1){
-            kill(users->userList[i].pid, SIGUSR1);
-            continue;
-        }
-        int written = write(fifo, &h, sizeof(Headers));
-        if(written < 0){
-            kill(users->userList[i].pid, SIGUSR1);
-        }
-    }
-
-    removeUser(users, pid);
-}*/
 
 int main(){
     setbuf(stdout, NULL);
