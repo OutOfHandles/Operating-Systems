c47ae6752feb146c351ef83d878f392b0a5eabc6
OutOfHandles <marcoandreportela2005@gmail.com>
2024-12-07 18:38:43 +0000

    moved to a different folder and improved error handling

--- Feed/feed.c
+++ Feed/feed.c
@@ -1,8 +1,15 @@
 #include "feed.h"
-#include "ultis.h"
+#include "../ultis.h"
 
 char feedFifo[50];
 int fd_manager_fifo, fd_feed_fifo;
+
+void Abort(int i){
+    close(fd_manager_fifo);
+    close(fd_feed_fifo);
+    unlink(feedFifo);
+    exit(i);
+}
 
 int sendToManager(Type t, void *data, int size){
     char bytes[MAX_BYTES];
@@ -47,7 +54,7 @@ void handleSignal(int sig, siginfo_t *info, void *secret){
     }
 }
 
-void* readManager(){
+void *readManager(){
     signal(SIGUSR1, handle_signal);
     Headers req;
     while(1){
@@ -58,6 +65,7 @@ void* readManager(){
             ServerMessage msg;
             size = read(fd_feed_fifo, &msg, sizeof(ServerMessage));
             if(size == -1) break;
+
             printf("%s\n", msg.message);
         }
     }
@@ -65,18 +73,11 @@ void* readManager(){
     pthread_exit(NULL);
 }
 
-void Abort(int i){
-    close(fd_manager_fifo);
-    close(fd_feed_fifo);
-    unlink(feedFifo);
-    exit(i);
-}
-
 int main(int argc, char *argv[]){
     setbuf(stdout, NULL);
     
     if(argc != 2){
-        printf("\nInvalid arguments\n");
+        printf("Invalid arguments\n");
         return 1;
     }
 
@@ -90,7 +91,7 @@ int main(int argc, char *argv[]){
     fd_manager_fifo = open(MANAGER_FIFO, O_WRONLY);
 
     if(fd_manager_fifo == -1){
-        printf("\nError opening manager\n");
+        printf("Error opening manager\n");
         exit(1);
     }
 
@@ -98,8 +99,8 @@ int main(int argc, char *argv[]){
 
     if(mkfifo(feedFifo, 0666) == -1){
         if(errno == EEXIST)
-            printf("\nFifo already created\n");
-        printf("\nError creating fifo\n");
+            printf("Fifo already created\n");
+        printf("Error creating fifo\n");
 
         close(fd_manager_fifo);
         exit(1);
@@ -114,49 +115,44 @@ int main(int argc, char *argv[]){
 
     if(sendToManager(LOGIN, argv[1], strlen(argv[1]) + 1) <= 0){
         printf("Failed to send to manager\n");
+        Abort(1);
     }
 
     ManagerStatus response;
-    read(fd_feed_fifo, &response, sizeof(ManagerStatus));
+    if(read(fd_feed_fifo, &response, sizeof(ManagerStatus)) == -1) Abort(1);
 
     switch(response){
         case SUCCESS: {
-            printf("\nSuccessfully logged in!\n");
+            printf("Successfully logged in!\n");
             break;
         }
         case MANAGER_FULL: {
-            printf("\nManager is full\n");
+            printf("Manager is full\n");
             Abort(1);
         }
         case TRUNCATED: {
-            printf("\nYour username has been truncated\n");
+            printf("Your username has been truncated\n");
             break;
         }
         case USERNAME_IN_USE: {
-            printf("\nUsername already in use\n");
+            printf("Username already in use\n");
             Abort(1);
         }
         default: {
-            printf("%d\n", response);
             break;
         }
     }
 
     pthread_t thread;
-    pthread_mutex_t mutex;
-    if(pthread_mutex_init(&mutex, NULL) != 0){
-        printf("Mutex init has failed\n");
-        Abort(1);
-    }
-
+    int running = 1;
     if(pthread_create(&thread, NULL, &readManager, NULL)!= 0){
         printf("Thread can't be created\n");
-        pthread_mutex_destroy(&mutex);
+        running = 0;
         Abort(1);
     }
 
     char command[500];
-    while(1){
+    while(running){
         int n_args = 0;
         if(fgets(command, BUFFER_SIZE, stdin) == NULL) break;
         if(command[strlen(command) - 1] == '\n') command[strlen(command) - 1] = '\0';
@@ -180,26 +176,30 @@ int main(int argc, char *argv[]){
                 break;
             }
 
-            sendToManager(ZERO, &data, sizeof(Zero));
+            if(sendToManager(ZERO, &data, sizeof(Zero)) <= 0){
+                printf("Failed to send request to manager try again\n");
+            }
         }
         else if(n_args == 1){
             One data;
             getCommandName(command, data.command);
             getArg(command, data.arg);
             
-            sendToManager(ONE, &data, sizeof(One));
+            if(sendToManager(ONE, &data, sizeof(One)) <= 0){
+                printf("Failed to send request to manager try again\n");
+            }
         }
         else if(n_args == 3){
             Three data;
             getArg(command, data.topic);
             getLifeTime(command, &data.lifetime);
             getMessage(command, data.message);
-            sendToManager(THREE, &data, sizeof(Three));
+            if(sendToManager(THREE, &data, sizeof(Three)) <= 0){
+                printf("Failed to send request to manager try again\n");
+            }
         }
     }
-
     pthread_kill(thread, SIGUSR1);
     pthread_join(thread, NULL);
-    pthread_mutex_destroy(&mutex);
     Abort(0);
 }
